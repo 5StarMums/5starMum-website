9c8f779e5b466bdde3fdd603f92cc1e43df3838e
Zeezart <azeezhazeezah010@gmail.com>
2023-03-30 14:58:12 +0100

    Feat:created website page

---
+++ 5Starproject/src/components/comments.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function Comments(){
+
+    // HANDLING THE SLIDER ICONS
+
+    function slideLeft(){
+        var slider = document.getElementById("slider")
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
+
+    function slideRight(){
+        var slider = document.getElementById("slider")
+        slider.scrollLeft = slider.scrollLeft + 500
+    }
+
+    return(
+        <div className='comments-container'>
+            <div id='slider' className='comments'>
+                <div className='individual-comments'>
+                    <img src='images/quote.svg' className='quote' />
+                    <p className='main-comment'>I've had more support in this community than any Facebook group I am on.</p>
+                    <div className='comment-details'>
+                        <img src='images/profile.svg' className='profile' />
+                        <div className='profile-info'>
+                            <p className='profile-name'><b>Mary Silva</b></p>
+                            <p className='profile-status'>Mother of 3</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='individual-comments'>
+                    <img src='images/quote.svg' className='quote' />
+                    <p className='main-comment'>I like it here! The women are so supportive and Mya is always there for me.</p>
+                    <div className='comment-details'>
+                        <img src='images/profile2.svg' className='profile' />
+                        <div className='profile-info'>
+                            <p className='profile-name'><b>Jane Bill</b></p>
+                            <p className='profile-status'>Expecting Baby</p>
+                        </div>
+                    </div>
+                </div> 
+
+                <div className='individual-comments'>
+                    <img src='images/quote.svg' className='quote' />
+                    <p className='main-comment'>After my first postpartum depression, I found this community</p>
+                    <div className='comment-details'>
+                        <img src='images/profile3.svg' className='profile' />
+                        <div className='profile-info'>
+                            <p className='profile-name'><b>Abbi Tessa</b></p>
+                            <p className='profile-status'>Mother of 1</p>
+                        </div>
+                       
+                    </div>
+                </div> 
+
+                {/* <div className='individual-comments'>
+                    <img src='images/quote.svg' className='quote' />
+                    <p className='main-comment'>After my first postpartum depression, I found this community</p>
+                    <div className='comment-details'>
+                        <img src='images/profile3.svg' className='profile' />
+                        <p className='profile-name'>Mary Silva</p>
+                        <p className='profile-status'>Mother of 3</p>
+                    </div>
+                </div>  */}
+            </div>
+
+            <FontAwesomeIcon onClick={slideLeft} icon={faAngleLeft} className='arrows'/>
+            <FontAwesomeIcon onClick={slideRight} icon={faAngleRight} className='arrows'/>
+        </div> 
+    )
+}
+
---
+++ 5Starproject/src/lists/questions.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+        id: 1,
+        question: "Do I have to pay?",
+        ans: "Yes but only to enjoy certain features. Particpation in the community is free forever. Even better, 5StarMums will earn you points when you refer others."
+    },
+
+    {
+        id: 2,
+        question: "How will i join the community?",
+        ans: "Join the waitlist and you'll be enlisted as soon as the community is launched."
+    },
+
+    {
+        id: 3,
+        question: "When will the AI be available?",
+        ans: "You'll be the first to know when the AI is available because you'll get a mail."
+    }
+]
+
+export default questions;
